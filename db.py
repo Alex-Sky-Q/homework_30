515a366370f2bcdc52736804f386fea40a84d773
Alex-Sky-Q <83851934+Alex-Sky-Q@users.noreply.github.com>
2026-03-01 21:43:29 +0300

    Update db.py

--- db.py
+++ db.py
@@ -22,7 +22,9 @@ class Base(DeclarativeBase):
 
 
 # Таблица для связи рецептов и ингредиентов
-recipe_ingredient_link = Table('recipe_ingredient', Base.metadata,
+recipe_ingredient_link = Table(
+    'recipe_ingredient',
+    Base.metadata,
     Column('recipe_id', ForeignKey('recipe.id'), primary_key=True),
     Column('ingredient_id', ForeignKey('ingredient.id'), primary_key=True))
 
@@ -36,8 +38,8 @@ class Recipe(Base):
     cook_time: Mapped[int | None] = mapped_column(Integer, nullable=True)
     description: Mapped[str | None] = mapped_column(Text, nullable=True)
     # Связь с ингредиентами
-    ingredients: Mapped[list["Ingredient"]] = relationship(secondary=recipe_ingredient_link, back_populates="recipes",
-                                                           lazy="selectin")
+    ingredients: Mapped[list["Ingredient"]] = relationship(
+        secondary=recipe_ingredient_link, back_populates="recipes", lazy="selectin")
 
     def __repr__(self):
         return f"<Recipe(id={self.id}, name='{self.name}')>"
@@ -49,9 +51,8 @@ class Ingredient(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     # Связь с рецептами
-    recipes: Mapped[list["Recipe"]] = relationship(secondary=recipe_ingredient_link, back_populates="ingredients")
+    recipes: Mapped[list["Recipe"]] = relationship(
+        secondary=recipe_ingredient_link, back_populates="ingredients")
 
     def __repr__(self):
         return f"<Ingredient(id={self.id}, name='{self.name}')>"
-
-
